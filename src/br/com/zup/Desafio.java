8b094667b47131e79e77ecde14ccea08bad36910
Bárbara Romero <bqromero@gmail.com>
2021-09-03 14:07:08 -0300

    Criando menu de opções

--- src/br/com/zup/Desafio.java
+++ src/br/com/zup/Desafio.java
@@ -1,21 +1,5 @@
 package br.com.zup;
 
-/*Crie um programa para gerenciar uma lista de funcionários de uma empresa,
-cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser
-exibida a lista de funcionários cadastrados.
-
-Entrega Mínima: O sistema permite, via terminal, inserir pelo menos 1 funcionário
-em uma lista e retornar o mesmo na tela.
-
-Entrega Média: O sistema permite adicionar mais de um funcionário e apresenta
-um menu para decidir se deverá:
-1. Adicionar mais funcionários.
-2. Exibir a lista de todos os funcionários cadastrados.
-3. Encerrar o programa.
-
-Entrega Máxima: O Sistema permite excluir um funcionário usando como
-parâmetro o CPF e não permite inserir um funcionário com o CPF repetido.*/
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -26,24 +10,69 @@ public class Desafio {
     Scanner leitor = new Scanner(System.in);
     HashMap<String, String> funcionarios = new HashMap<String, String>();
 
+    //Declarando variáveis
+
+    int contador = 1;
+    int voltarAoMenu = 0;
+    int opcaoEscolhida = 0;
+    String nomeDoFuncionario = "";
+    String telefoneDoFuncionario = "";
+    String emailDoFuncionario = "";
+    String cpfDoFuncionario = "";
+
     //Apresentando o programa
+
     System.out.println("Bem vinde! Este programa quer te auxiliar a criar uma lista de funcionários, que possa ser gerenciada por você sem dificuldades.");
     System.out.println("Vamos começar?");
 
-    //Inserindo dados do funcionário, cadastrando em uma lista e mostrando para o usuário
-    System.out.println("Nome completo do funcionário: ");
-    String nomeDoFuncionario = leitor.nextLine();
-    System.out.println("Telefone do funcionário: ");
-    String telefoneDoFuncionario = leitor.nextLine();
-    System.out.println("E-mail do funcionário: ");
-    String emailDoFuncionario = leitor.nextLine();
-    System.out.println("CPF do funcionário: ");
-    String cpfDoFuncionario = leitor.nextLine();
+    //Criando menu e inserindo mais funcionários
 
-    funcionarios.put(cpfDoFuncionario, " Nome: " + nomeDoFuncionario + " Telefone: " + telefoneDoFuncionario + " Email: " +emailDoFuncionario);
-    System.out.println(funcionarios);
+    do {
+      System.out.println(""); //pulando uma linha
+      System.out.println("MENU - Considere as opções a seguir:");
+      System.out.println("1 - Inserir novo funcionário;");
+      System.out.println("2 - Exibir lista completa de funcionários cadastrados;");
+      System.out.println("3 - Encerrar programa.");
+      System.out.println("0 - Voltar ao menu principal");
+      System.out.println(""); //pulando uma linha
+      System.out.println("Por favor, digite a opção desejada: ");
+      opcaoEscolhida = leitor.nextInt();
 
+      leitor.nextLine();
+
+      switch (opcaoEscolhida) {
+        case 1:
+          System.out.println("Nome completo do funcionário: ");
+          nomeDoFuncionario = leitor.nextLine();
+          System.out.println("Telefone do funcionário: ");
+          telefoneDoFuncionario = leitor.nextLine();
+          System.out.println("E-mail do funcionário: ");
+          emailDoFuncionario = leitor.nextLine();
+          System.out.println("CPF do funcionário: ");
+          cpfDoFuncionario = leitor.nextLine();
+          funcionarios.put(cpfDoFuncionario, " Nome: " + nomeDoFuncionario + " Telefone: " + telefoneDoFuncionario + " Email: " + emailDoFuncionario);
+
+          System.out.println("Para voltar ao menu, digite 0");
+          voltarAoMenu = leitor.nextInt();
+          break;
+
+        case 2:
+          for (String chaveCPF : funcionarios.keySet()) {
+            System.out.println(funcionarios.get(chaveCPF) + chaveCPF);
+          }
+          System.out.println(funcionarios);
+          break;
+
+        case 3:
+          System.out.println("O programa foi encerrado!");
+          System.out.println(""); //pulando uma linha
+          System.out.println("DESEJA RETORNAR?");
+      }
+
+    } while (voltarAoMenu == 0);
 
 
   }
 }
+
+
